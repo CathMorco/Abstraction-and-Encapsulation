5bf80dc22c21a358d8be476caa1c9635c3d1c3b1
unknown <morco.cathleenjan.12192003@gmail.com>
2023-07-02 15:44:11 +0800

    Added appropriate comments for each function, removed the unnecessary directions at the start of the code, created a new program TestFan, and moved the main program over there

---
+++ TestFan.py
@@ -0,0 +1,9 @@
+from FanClass import Fan
+import sys
+from PyQt5.QtWidgets import QApplication, QMessageBox
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    Fan1 = Fan(3,10,"Yellow", 100, 100)
+    Fan2 = Fan(2,5,"Blue", 500, 100)
+    sys.exit(app.exec_())
